79d392da7c831437f11d2679d5f80f897bd90ab6
ayirezang <patienceayirezang33@gmail.com>
2026-08-05 22:19:12 +0000

    low stock product function

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,4 @@
+import { trusted } from "mongoose";
 import Product from "../models/product.js";
 
 //create a new product
@@ -44,62 +45,41 @@ export const getProductById = async (req, res) => {
 };
 
 // Update a product by ID
-
-export const updateProductById = async (req, res) => {
+export const updateProduct = async (req, res) => {
   try {
-    // Get the product ID from the request parameters
-    const id = req.params.id;
-    {
-      /**extract the updated product data from the request body */
+    const { id } = req.params;
+    const { amount } = req.body;
+    if (typeof amount !== "number") {
+      return res.status(400).json({ message: "Invalid amount" });
     }
-    const updatedProductData = req.body;
-    {
-      /**update the product in the database */
-    }
-    const product = await Product.findById(id);
-
-    if (!product) {
-      res.status(404).json({ message: "Product not found" });
-    }
-    product.name = updatedProductData || product.name;
-    product.price = updatedProductData.price || product.price;
-    product.quantity = updatedProductData.quantity || product.quantity;
-    product.supplier = updatedProductData.supplier || product.supplier;
-    product.description = updatedProductData.description || product.description;
-    {
-      /**save the updated product */
-    }
-    const updatedProduct = await product.save();
-
-    res.status(200).json({ message: "Product updated successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-//delete
-export const deleteProduct = async (req, res) => {
-  try {
-    const id = req.params.id;
-    {
-      /**find the product to delete */
-    }
-    const deletedProduct = await Product.findByIdAndDelete(id);
-    if (!deletedProduct) {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      { $inc: { quantity: amount } },
+      { new: true, runVlidators: true },
+    );
+    if (!updatedProduct) {
       return res.status(404).json({ message: "Product not found" });
     }
-    res.status(200).json({ message: "Product deleted successfully" });
+    return res.stauts(200).json(updatedProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 //low stock alert
-export const lowStockAlert = async (req, res) => {
+export const getLowStockProducts = async (req, res) => {
   try {
-    const lowStockProducts = await Product.find({
-      quantity: { $lt: 10 },
+    {
+      /*check if threshold has been passed or use 10*/
+    }
+    const threshold = Number(req.query.threshold) || 10;
+    {
+      /**query mongobd $lt */
+    }
+    const lowStockProductss = await Product.find({
+      quantity: { $lt: threshold },
     });
-    res.status(200).json(lowStockProducts);
+    return res.status(200).json("low stock products").json(lowStockProductss);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
